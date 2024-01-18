01a6c23ecef7c91a9f67edb3388e93e32924b00d
Samarth <nimbargisamarth@gmail.com>
2024-01-18 23:11:37 +0530

    commiting before installing tailwind

--- portal/static/portal/users-database.js
+++ portal/static/portal/users-database.js
@@ -1,6 +1,11 @@
 
 function showModal(){
   const modal = document.getElementById('modal-sam')
+  const add_user = document.getElementById('add-user')
+  const modal_button = document.getElementById('modal-button')
+
+  add_user.disabled = true;
+  modal_button.disabled = true;
 
   modal.classList.remove('hidden');
   modal.classList.add('flex')
@@ -9,6 +14,11 @@ function showModal(){
 document.getElementById('hide-modal').addEventListener('click', (e) => {
   e.preventDefault();
   const modal = document.getElementById('modal-sam')
+  const add_user = document.getElementById('add-user')
+  const modal_button = document.getElementById('modal-button')
+
+  add_user.disabled = false;
+  modal_button.disabled = false;
 
   modal.classList.remove('flex');
   modal.classList.add('hidden')
