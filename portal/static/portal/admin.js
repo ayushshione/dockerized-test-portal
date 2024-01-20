beee85d9a565051591e9adac8a2d0f83d5edff55
Samarth <nimbargisamarth@gmail.com>
2024-01-21 01:42:58 +0530

    added delete test modal

--- portal/static/portal/admin.js
+++ portal/static/portal/admin.js
@@ -13,44 +13,27 @@ function getCSRFToken() {
   return csrfToken;
 }
 
+function showModal(event, id) {
+  event.stopPropagation();
+  const modal = document.getElementById("modal-sam");
 
-const timeInput = document.getElementById("time-test");
-const butt = document.getElementById('butt');
-const spinnerWrapperEl = document.querySelector('.spinner-wrapper')
+  modal.setAttribute("data-id", id);
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+}
 
+document.getElementById("hide-modal").addEventListener("click", (e) => {
+  e.preventDefault();
+  const modal = document.getElementById("modal-sam");
 
-// Array.prototype.forEach.call(collections, (timeInput) => {
-  butt.addEventListener("click", function () {
-    spinnerWrapperEl.style.display = "flex";
-    time_now = timeInput.value;
-    const test_id = timeInput.getAttribute("data-mydata");
+  modal.classList.remove("flex");
+  modal.classList.add("hidden");
+});
 
-    fetch(`/basic-settings/change-time/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRFToken": getCSRFToken(),
-      },
-      body: JSON.stringify({
-        test_id: test_id,
-        time_now: time_now,
-      }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        spinnerWrapperEl.style.display = 'none';
-        console.log(data);
-      })
-      .catch((error) => {
-        // Handle errors
-        console.error("There was a problem with the fetch operation:", error);
-      });
+document.getElementById("delete-test-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  const modal = document.getElementById("modal-sam");
 
-    console.log("Time changed:", timeInput.value);
-  });
-// });
+  const id = parseInt(modal.getAttribute("data-id"));
+  window.location.href = `/delete-test/${id}`;
+});
