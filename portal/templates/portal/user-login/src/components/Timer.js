7c07ef069ff15e18754ad5bd72dffc56e19c3801
samarth4445 <nimbargisamarth@gmail.com>
2024-01-25 19:43:05 +0530

    ni

--- portal/templates/portal/user-login/src/components/Timer.js
+++ portal/templates/portal/user-login/src/components/Timer.js
@@ -12,18 +12,14 @@ const CountdownTimer = ({ remainingTime, onTimerEnd }) => {
     if (time === null) return;
 
     const intervalId = setInterval(() => {
-      // Decrease time by 1 second
       setTime(prevTime => prevTime - 1);
 
-      // Check if the timer has reached zero
       if (time === 0) {
         clearInterval(intervalId);
-        // Call the callback function when the timer reaches zero
         onTimerEnd();
       }
     }, 1000);
 
-    // Cleanup the interval when the component is unmounted
     return () => clearInterval(intervalId);
   }, [time, onTimerEnd]);
 
@@ -31,12 +27,10 @@ const CountdownTimer = ({ remainingTime, onTimerEnd }) => {
     return <p>Loading...</p>;
   }
 
-  // Calculate hours, minutes, and seconds
   const hours = Math.floor(time / 3600);
   const minutes = Math.floor((time % 3600) / 60);
   const seconds = time % 60;
 
-  // Format the time as HH:MM:SS
   const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 
   return (
